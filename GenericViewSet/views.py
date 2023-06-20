57a213d7dae13161036e049421838f24c5bf4554
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-19 20:39:46 -0400

    def as_view(cls, actions=None, **initkwargs):

--- GenericViewSet/views.py
+++ GenericViewSet/views.py
@@ -33,3 +33,152 @@ view_is_async = False
 
 def _allowed_methods(self):
     return [m.upper() for m in self.http_method_names if hasattr(self, m)]
+
+
+@classmethod
+def as_view(cls, actions=None, **initkwargs):
+    """
+    Because of the way class based views create a closure around the instantiated view, we need to totally reimplement
+    '.as_view', and slightly modify the view function that is created and returned.
+    """
+    # The name and description initkwargs may be explicitly overridden for
+    # certain route configurations. eg, names of extra actions.
+    cls.name = None
+    cls.description = None
+    # The suffix initkwarg is reserved for displaying the viewser tepe.
+    # This initkwarg should have no effect if the name is provided.
+    # eg. 'List' or 'Instance'.
+    cls.suffix = None
+    # the detail initkwarg is reserved for introspecting the viewset type.
+    cls.detail = None
+    # Setting a basename by the router through the initkwargs.
+    # value is provided by the router through the initkwargs.
+    cls.basename = None
+    # actions must not be empty
+    if not actions:
+        raise TypeError("The 'actions' argument must be provided when"
+                        "calling '.as_view()' on a ViewSet. For example"
+                        "'.as_view({'get': 'list'})'")
+    # sanitize keyword arguments
+    for key in initkwargs:
+        if key in cls.http_method_names:
+            raise TypeError("You tried to pass in the %s method name as a"
+                            "keyword argument to %s(). Don't do that."
+                            %(key, cls.__name__))
+        if not hasattr(cls, key):
+            raise TypeError("%s() received an invalid keyword %r" %(
+                cls.__name__, key))
+    # name and suffix are mutually exclusive
+    if 'name' in initkwargs and 'suffix' in initkwargs:
+        raise TypeError("%s() received both 'name' and 'suffix', which are"
+                        "mutually exclusive arguments." % (cls.__name__))
+    def view(request, *args, **kwargs):
+        self = cls(**initkwargs)
+
+        if 'get' in actions and 'head' not in actions:
+            actions['head'] = actions['get']
+
+        # We also store the mapping of request methods to actions,
+        # so that we can later set the action attribute
+        # eg. 'self.action - 'list' on an incoming GET request.
+        self.action_map = actions
+
+        # Bind methods to actions
+        # This is the bit that's different to a standard view
+        for method, action in actions.items():
+            handler = getattr(self, action)
+            setattr(self, method, handler)
+
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+
+        # And continue as usual
+        return self.dspatch(request, *args, **kwargs)
+
+    # take name and docstring from class
+    update_wrapper(view, cls, updated=())
+
+    # and possible attributes set by decorators
+    # like csrfO_exempt from dispatch
+    update_wrapper(view, cls.dispatch, assigned=())
+
+    # we need to set these on the view function, so that breadcrumb
+    # generation can pick out these bits of information from a
+    # resolved URL
+    view.cls = cls
+    view.initkwargs = initkwargs
+    view.actions = actions
+    return csrf_exempt(view)
+
+
+
+
+@classmethod
+def as_view(cls, **initkwargs):
+    """
+    Store the original class on the view function.
+
+    This allows us to discover information about the view when we do URL
+    reverse lookups. Used for breadcrumb generation.
+    """
+    if isinstance(getattr(cls, 'queryset', None), models.query.QuerySet):
+        def force_evaluation():
+            raise RuntimeError(
+                "Do not evaluate the '.queryset' attribute directly, "
+                "as the result will be cached and reused between requests."
+                "Use '.all()' or call '.get_queryset()' instead."
+            )
+        cls.queryset._fetch_all = force_evaluation
+
+    view = super().as_view(**initkwargs)
+    view.cls = cls
+    view.initkwargs = initkwargs
+
+    # Note: session based authentication is explicitly CSRF validated,
+    # alL other authentication is CSRF exempt.
+    return csrf_exempt(view)
+
+
+@classmethod
+def as_view(cls, **initkwargs):
+    """ Main entry point for a request-response process."""
+    for key in initkwargs:
+        if key in cls.http_method_names:
+            raise TypeError(
+                "The method name %s is not accepted as a keyword argument "
+                " to %s()." %(key, cls.__name__)
+                 )
+        if not hasattr(cls, key):
+            raise TypeError(
+                "%s() received an invalid keyword %r. as_view"
+                 "only accepts argument that are already"
+                "attributes of the class." %(cls.__name__, key)
+            )
+
+    def view(request, *args, **kwargs):
+        self = cls(**initkwargs)
+        self.setup(request, *args, **kwargs)
+        if not hasattr(self, 'request'):
+            raise AttributeError(
+                "%s instance has no 'request' attribute. Did you override"
+                "setup() and forget to call super()?" %cls.__name__
+            )
+        return self.dispatch(request, *args, **kwargs)
+
+    view.view_class = cls
+    view.view_initkwargs = initkwargs
+
+    # __name__and __qualname__ are intentionally left unchanged as
+    # view_class should be used to robustly determine the name of the view
+    # instead.
+    view.__doc__ = cls.__doc__
+    view.__module__ = cls.__module__
+    view.__annotaions__ = cls.dispatch__annotations__
+    # Copy possible attributes set by decorators, e.g @csrf_exempt, from
+    # the despatch method/
+    view.__dict__.update(cls.dispatch.__dict__)
+    # Mark th callback if the view class is async.
+    if cls.view_is_async:
+        view._is_coroutine = asyncio.coroutines._is_coroutine
+    return view
